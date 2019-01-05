2698a35d103a0fb76160b309bf1ea57a10c276b9
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-01-05 20:26:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -628,4 +628,12 @@ client.on('message',function(message) {
    }
 });
 
+client.on('message', async message => {
+          if(message.content.startsWith(prefix + "active")) {
+              if(!message.channel.guild) return message.reply message.channel.send("** هذا الامر للسيرفرات فقط ⛔ ** ");
+            message.member.addRole(message.guild.roles.find("name". "Active"));
+            message.author.send("** تم تفعيلك استمتع 🙂 **")
+          }
+        });
+
 client.login(process.env.BOT_TOKEN);
